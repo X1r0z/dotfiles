2b7f8231601db9a8d08f84aa7fb28f64386ae702
X1r0z <i@exp10it.io>
2024-10-29 18:15:41 +0800

    2024-10-29

--- setup.sh
+++ setup.sh
@@ -3,3 +3,13 @@
 # vim-plug for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# fix cursor in vim terminal
+
+if [ -n "$BASH_VERSION" ]; then
+    echo 'echo -ne "\e[3 q"' >> ~/.bashrc
+fi
+
+if [ -n "$ZSH_VERSION" ]; then
+    echo 'echo -ne "\e[3 q"' >> ~/.zshrc
+fi
